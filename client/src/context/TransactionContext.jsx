77a75bd0f41972ff677bb70630acb6cae18f8538
lila <nikonova@hotmail>
2022-01-02 17:29:22 +0100

    interaction with smart_contract works

---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,5 @@
+// Transaction deploy to: 
+//0x9246E0b917D307A6265f3B00f25a9413d4dAf732
+
+
+
---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x9246E0b917D307A6265f3B00f25a9413d4dAf732';
+
