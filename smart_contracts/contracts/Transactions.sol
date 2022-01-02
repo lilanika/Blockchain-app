bb358e7c3ff5e9320d6ab4cb27afd17f40c57dd6
lila <nikonova@hotmail>
2022-01-02 14:58:17 +0100

    created my first smart_contract

--- smart_contracts/contracts/Transactions.sol
+++ smart_contracts/contracts/Transactions.sol
@@ -1,5 +1,3 @@
-
-```
 //SPDX-License-Identifier: UNLICENSED
 //choosing a version of solidity 
 pragma solidity ^0.8.0;
@@ -7,7 +5,7 @@ pragma solidity ^0.8.0;
 
 contract Transactions {
 //first  is the type second the var
- uint256 transactionCounter;
+ uint256 transactionCount;
 
 //later the event has to be emitted from somewhere
  event Transfer(
@@ -34,19 +32,25 @@ string keyword;
 //this means the transactions is going to be an array. This meaning it will be an array of objects. 
 TransferStruct[] transactions; 
 
-//public means every can access this function. 
-function addToBlockchain() public {
- 
+//public means every can access this function.
+//will return nothing just doing something 
+function addToBlockchain(address payable receiver, uint amount, 
+string memory message, string memory keyword ) public {
+ transactionCount += 1; 
+ transactions.push(TransferStruct( msg.sender, receiver, amount,message, block.timestamp, keyword));
+
+ emit Transfer(msg.sender, receiver, amount,message, block.timestamp, keyword);
+
 }
 
-//this function return 
+
 function getAllTransactions() public view returns (TransferStruct[] memory) {
-  //return transaactions
+  return transactions;
  
 }
 
 function getTransaactionCount() public view returns (uint256) {
-  //return transactionCount
+  return transactionCount;
  
 }
 
