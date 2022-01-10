4a571083c83caa881702e787cdafba7a7c542607
lila <nikonova@hotmail>
2022-01-10 13:50:19 +0100

    adding dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Blockchain-app/',
+ base: '/Blockchain-app/',
   plugins: [react()]
 })
